da935459d87a486c999be002a02e68619b47f214
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-02-08 15:52:47 +0200

    Update

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open('README.md', 'r') as f:
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 setup(
-    name="ClientWars",  # How you named your package folder
-    packages=["ClientWars"],  # Chose the same as "name"
+    name="ClientWars",
+    packages=["ClientWars"],
     include_package_data=True,
-    version="v1.0",  # Start with a small number and increase it with every change you make
-    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description="War game, but for programmers.",  # Give a short description about your library
+    version="v1.0",
+    license="MIT",
+    description="War game, but for programmers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="yoyo",  # Type in your name
-    author_email="serverwars00@gmail.com",  # Type in your E-Mail
-    url="https://github.com/hvuhsg/ClientWars",  # Provide either the link to your github or to your website
+    author="yoyo",
+    author_email="serverwars00@gmail.com",
+    url="https://github.com/hvuhsg/ClientWars",
     download_url="",
     keywords=[
         "game",
@@ -23,12 +23,12 @@ setup(
         "programming",
         "win"
     ],  # Keywords that define your package best
-    install_requires=requirements,  # I get to this in a second
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
